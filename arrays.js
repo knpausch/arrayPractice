e623315b71a270e9f74d82e917223470b9b9949d
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-07-08 12:06:50 -0700

    Added 3 methods with explainations in comments

--- arrays.js
+++ arrays.js
@@ -1,9 +1,15 @@
-var simpsons = ["Homer","Marge","Lisa","Bart","Maggie"]
+var simpsons = ["Homer","Marge","Lisa","Bart","Maggie"];
+//Removes last element from array
+simpsons.pop();
 
-var donutBoxes = [4,2,8,0,8,1]
+var donutBoxes = [4,2,8,0,8,1];
+//Removes first element in array
+donutBoxes.shift();
 
-var saidDoh = [true, false, false, false, true]
+var saidDoh = [true, false, false, false, true];
+//Adds a given element into end of array
+saidDoh.push(true);
 
-console.log(simpsons)
-console.log(donutBoxes)
-console.log(saidDoh)
+console.log(simpsons);
+console.log(donutBoxes);
+console.log(saidDoh);
