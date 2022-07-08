f9f55e63b45dc8c476b6ac79e43259230dbf613c
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-07-08 12:01:22 -0700

    Added simpson names, number of donut boxes and if doh was said

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var simpsons = ["Homer","Marge","Lisa","Bart","Maggie"]
+
+var donutBoxes = [4,2,8,0,8,1]
+
+var saidDoh = [true, false, false, false, true]
+
+console.log(simpsons)
+console.log(donutBoxes)
+console.log(saidDoh)
